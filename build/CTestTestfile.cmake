89044a39cb3df755bd9acbba943971aad981e10a
bryantla <ee106a-aca@c105-7.local>
2022-11-14 15:58:31 -0800

    catkin_make'd

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/cc/ee106a/fa22/class/ee106a-aca/C106A_Pendulum/src
+# Build directory: /home/cc/ee106a/fa22/class/ee106a-aca/C106A_Pendulum/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("pendulum_pkg")
